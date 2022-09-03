1c71a00dc44fe3a29ce56463e2fcee21531841f9
Lin2266 <Q0961136702@gmail.com>
2022-09-03 15:06:26 +0800

    add 履歷範本分類、按鈕、js跟css共用連結

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ function f(p1 , p2){
 }
 
 console.log(f(10 , 20));
+
+$(function(){
+   let category = document.getElementsByClassName('btn13');
+   for(let i = 0; i < category.length; i++){
+      category[i].addEventListener('click',function(){
+         console.log(category[i])
+         $(this).css('background-color','#EAA565').css('color','#FFFFFF')
+      })
+   }
+   
+})
