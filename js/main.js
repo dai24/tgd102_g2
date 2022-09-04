eb5d9ace4475c5e9dfce2961da8905961715d26e
Lin2266 <Q0961136702@gmail.com>
2022-09-05 01:28:24 +0800

    add pagination.html分頁頁碼 by lin

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-function f(p1 , p2){
-   return p1 * p2 
-}
-
-console.log(f(10 , 20));
-
 $(function(){
    // 履歷製作按鈕連結
    $('#resume_btn').on('click',function(){
@@ -11,15 +5,14 @@ $(function(){
    })
 
    // 履歷範本分類按鈕(未完成)
-   // let category = document.getElementsByClassName('btn13');
-   // for(let i = 0; i < category.length; i++){
-   //    category[i].addEventListener('click',function(){
-   //       console.log(category[i])
-   //       $(this).css('background-color','#EAA565').css('color','#FFFFFF')
-   //    })
-   // }
    $('.btn13').on('click',function(){
       $(this).css('background-color','#EAA565').css('color','#FFFFFF')
    })
    
 })
+
+// new Vue({
+//    el: '#app',
+//    data: {     // 變數放這裡
+//    }
+// })
