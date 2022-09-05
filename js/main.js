1dfe25ec8db2b2c3bce711b8c6197bb68e92df93
Lin2266 <Q0961136702@gmail.com>
2022-09-05 21:40:19 +0800

    update 麵包屑顏色、間距、頁碼class by lin

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-$(function(){
+$(function () {
    // 履歷製作按鈕連結
-   $('#resume_btn').on('click',function(){
+   $('#resume_btn').on('click', function () {
       location.href = "../resume_work_space.html"
    })
 
    // 履歷範本分類按鈕(未完成)
-   $('.btn13').on('click',function(){
-      $(this).css('background-color','#EAA565').css('color','#FFFFFF')
+   $('.btn13').on('click', function () {
+      $('.btn13').css('background-color', 'rgba(255, 255, 255, 0)').css('color', '#313131')
+      $(this).css('background-color', '#EAA565').css('color', '#FFFFFF')
    })
-   
+
+   // 頁碼
+   $('.pagination-ul a').on('click', function () {
+      $('.pagination-ul a').css('color', '#313131')
+      $(this).css('color', '#EAA565')
+   })
+
+   // 履歷放大
+   $('.resume_zoom_btn').on('click', function () {
+      let img_src = $(this).parents('.resume_card').children("img").attr('src')
+      
+   })
+
 })
 
 // new Vue({
