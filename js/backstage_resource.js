2e9791624c04029bfd4ef55184efd82b711ad6c2
North-Wang <b10234033.ntust@gmail.com>
2022-09-11 16:36:23 +0800

    更新 backstage_resume.html 王

---
+++ js/backstage_resource.js
@@ -0,0 +1,17 @@
+//swiper套件
+
+// var swiper = new Swiper('.mySwiper', {
+//     slidesPerView: 1,
+//     slidesPerColumn: 2,
+//     spaceBetween: 10,
+//     // loop: true,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//   });
+
