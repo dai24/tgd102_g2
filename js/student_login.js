c8279919a56df15c508b43e414ab2b6d644e9c21
North-Wang <b10234033.ntust@gmail.com>
2022-09-16 10:33:50 +0800

    update company_main.html 王

--- js/student_login.js
+++ js/student_login.js
@@ -1,15 +1,10 @@
 //按下"送出"按鈕，跳轉頁面
 let submit = document.querySelector(".submit");
-
 submit.addEventListener("click", () => {
     location = "./student_main.html";
-<<<<<<< HEAD
 })
 
 let submit2 = document.querySelector(".submit2");
-
 submit2.addEventListener("click", () => {
     location = "./student_sign.html";
-=======
->>>>>>> dev
 })
