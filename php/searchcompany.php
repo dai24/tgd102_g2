1e1fa7f5b928ebbcb3725035654a274602aba471
North-Wang <b10234033.ntust@gmail.com>
2022-09-20 17:36:43 +0800

    update searchCompany.php

--- php/searchcompany.php
+++ php/searchcompany.php
@@ -5,31 +5,32 @@
 
     //建立SQL語法
     $member = json_decode(file_get_contents("php://input"), true); //接收前端傳來的json格式
-    $sql = "select * from company"; 
+    $sql = "select 
+                c.id, c.name, c.address, c.property, c.principle, c.city, c.district,
+                c.address, c.create_date, d.fee_id 
+            from company c
+                join COMPANY_COIN_DETAILS d
+                on c.id = d.company_id;
+            "
+            ;    
 
     //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
     
     $stmt = $pdo->prepare($sql);
- 
-    // echo "測試有跑到這裡";
+    
     $stmt->execute(); //執行
     
     
     //---------------------------------------------------
     
     $members = $stmt->fetchAll();
-    
-    if ($stmt->rowCount() > 0){ //只要有撈到一筆資料 = 有撈到資料
-        $member = $members[0];        
-        $member["successful"] = true;
-        session_start(); //啟用session 
-        $_SESSION["loggedin"] = true;
-        $_SESSION["member"] = (object) $member;
         
+    if ($stmt->rowCount() > 0){ //只要有撈到一筆資料 = 有撈到資料
+        $member["successful"] = true; //successful的屬性數值顯示 true        
     } else{ //如果沒有撈到資料...
         $member["successful"] = false;
-        $member["message"] = "撈取失敗";
     }
     
-    echo  json_encode($member);
+    echo "測試有跑到這裡";
+    echo  json_encode($members);
 ?>
