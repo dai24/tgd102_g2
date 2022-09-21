07f0f6b2ee30b908aa88c117b0511edad128cf08
theell3 <the.ell3.com@gmail.com>
2022-09-21 19:26:08 +0800

    將resumeReviewtest.js 改寫為正確 by chen

--- js/resumeReviewtest.js
+++ js/resumeReviewtest.js
@@ -4,7 +4,8 @@ new Vue({
         isshow1:false,
         isshow2:false,
 
-        industrytype:"擅長產業".
+        industrytype:"擅長產業",
+        jobclasstype:"擅長職務",
 
         allteachers:[
             {tname:'王博均',tjobtitle:'人資經理',ttimes:5,tschool:'國立台灣大學商研所',tjobname:'Jobs雜誌 專欄作家',industrytype:'電子科技',jobclasstype:'管理幕僚',tdescription:'只要透過深入訪談',timg:'../images/resumeReview/1.jpg',tlink:'../dist/consultant.html'},
@@ -35,6 +36,18 @@ new Vue({
             this.isshow2 = !this.isshow2
         },
         
+        industrytypeis(industrytypechoose){
+            this.industrytype = industrytypechoose;
+            this.isshow1 = !this.isshow1
+        },
+
+        jobclasstypeis(jobclasstypechoose){
+            this.jobclasstype = jobclasstypechoose;
+            this.isshow2 = !this.isshow2
+        },
+
+    
+
     },
 
     // computed:{
