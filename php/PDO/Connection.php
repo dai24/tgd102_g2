dd07a24a73048ce8dd2d609c70a54e5126888849
Lin2266 <Q0961136702@gmail.com>
2022-09-21 14:42:30 +0800

    update vue resume zoom by lin

--- php/PDO/Connection.php
+++ php/PDO/Connection.php
@@ -4,6 +4,12 @@ $db_user = "root";
 $db_pass = "password";
 $db_select = "interngo";
 
+// 團專上傳tibame的連線
+// $db_host = "127.0.0.1";
+// $db_user = "tibamefe_since2021";
+// $db_pass = "vwRBSb.j&K#E";
+// $db_select = "tibamefe_tgd102g2";
+
 $dsn = "mysql:host=".$db_host.";dbname=".$db_select.";charset=utf8";
 $pdo = new PDO($dsn,$db_user,$db_pass);
 
