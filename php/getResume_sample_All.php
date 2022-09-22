2ada082d0a04e0043194fa75e2d6cfd85f7e4371
Lin2266 <Q0961136702@gmail.com>
2022-09-22 16:55:41 +0800

    add CATEGORY by lin

--- php/getResume_sample_All.php
+++ php/getResume_sample_All.php
@@ -1,20 +1,29 @@
 <?php
 include('./PDO/Connection.php');
 
-
-$sql = "SELECT STUDENT_ID,LIKE_COUNT,IMG_PATH FROM RESUME WHERE MODEL = 2";
-$statement = $pdo->prepare($sql);
-$statement->execute();
-
-if($statement->rowCount() > 0){
-    $member['successful'] = true;
-    $member['message'] = "取得履歷範本資料成功";
+$category = isset($_GET['category'])?$_GET['category']:'';
+if(isset($_GET['category']) && $_GET['category'] != '全部'){   
+    $sql = "SELECT STUDENT_ID,LIKE_COUNT,IMG_PATH FROM RESUME WHERE MODEL = 2 AND CATEGORY = ?";
+   
 }else{
-    $member['successful'] = false;
-    $member['message'] = "取得覆歷範本資料失敗";
+    $sql = "SELECT STUDENT_ID,LIKE_COUNT,IMG_PATH FROM RESUME WHERE MODEL = 2";
+    
 }
- 
-echo json_encode($statement->fetchAll());
-
+    $statement = $pdo->prepare($sql);
+    if(isset($_GET['category']) && $_GET['category'] != '全部'){
+        $statement->bindValue(1,$category);
+    }
+    $statement->execute();
+    // $members = $statement->fetchAll();
+    if($statement->rowCount() > 0){
+        // $member = $members;
+        $member['successful'] = true;
+        $member['message'] = "取得履歷範本".$category."資料成功";
+    }else{
+        $member['successful'] = false;
+        $member['message'] = "取得覆歷範本".$category."資料失敗";
+    }
+    
+    echo json_encode($statement->fetchAll());
 
 ?>
