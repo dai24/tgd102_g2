53966ef94ec96a4c466105139525fcf4d76e8166
Lin2266 <Q0961136702@gmail.com>
2022-09-24 15:01:58 +0800

    update vue code resume model by lin

---
+++ php/getResume_model_All.php
@@ -0,0 +1,13 @@
+<?php
+include('./PDO/Connection.php');
+
+$model = isset($_GET['model'])?$_GET['model']:'';
+
+    $sql = "SELECT PRICE,UNLOCK_STATUS,FILE_NAME,IMG_PATH FROM RESUME WHERE MODEL = ?";
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(1,$model);
+    $statement->execute();
+
+    echo json_encode($statement->fetchAll());
+
+?>
