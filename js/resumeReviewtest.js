e9ec2c93dad32f616440e1e11d42eff52f3d5931
theell3 <the.ell3.com@gmail.com>
2022-09-26 11:34:08 +0800

    將履歷診療室改以組件形式呈現 BYCHEN

--- js/resumeReviewtest.js
+++ js/resumeReviewtest.js
@@ -1,4 +1,40 @@
 
+// 擅長產業項目
+Vue.component("allindustryclass", {
+    template:`<li><button class="btna22" @click="searchindustryclass(index)" v-for="(item,index) in allindustrytype">{{ item }}</button></li>`,
+    data(){
+        return{
+            allindustrytype:["電子科技","資訊","軟體","金融","休閒","出版","藝文相關","法律","顧問","研發","餐飲","旅遊"],
+        }   
+    },
+
+    methods:{
+        searchindustryclass(i){
+            this.$emit("industryclass", this.allindustrytype[i])
+        }
+    },
+
+})
+
+// 擅長職務項目
+Vue.component("alljobclass", {
+    template:`<li><button class="btna22" @click="searchjobclass(index)" v-for="(item,index) in alljobtype">{{ item }}</button></li>`,
+    data(){
+        return{
+            alljobtype:["管理幕僚","人資","金融","財會","貿易","客服","行銷","企劃","資訊","專案管理","顧問","保險"],
+        }   
+    },
+    
+    methods:{
+        searchjobclass(i){
+            this.$emit("jobclass", this.alljobtype[i])
+        }
+    },
+
+})
+
+
+// 父層
 new Vue({
     el: '#resumeReviewtest',
     data: {     
@@ -11,10 +47,6 @@ new Vue({
 
         allteachers:[
             // {tname:'王博均',tjobtitle:'人資經理',ttimes:5,tschool:'國立台灣大學商研所',tjobname:'Jobs雜誌 專欄作家',industrytype:'電子科技',jobclasstype:'管理幕僚',tdescription:'只要透過深入訪談',timg:'../images/resumeReview/1.jpg',tlink:'../dist/consultant.html'},
-            // {tname:'薛彥涵',tjobtitle:'人資經理',ttimes:5,tschool:'國立台灣大學商研所',tjobname:'Jobs雜誌 專欄作家',industrytype:'法律',jobclasstype:'管理幕僚',tdescription:'只要透過深入訪談',timg:'../images/resumeReview/2.jpg',tlink:'../dist/consultant.html'},
-            // {tname:'李凱宇',tjobtitle:'人資經理',ttimes:5,tschool:'國立台灣大學商研所',tjobname:'Jobs雜誌 專欄作家',industrytype:'電子科技',jobclasstype:'資訊',tdescription:'只要透過深入訪談',timg:'../images/resumeReview/3.jpg',tlink:'../dist/consultant.html'},
-            // {tname:'宋軒辰',tjobtitle:'人資經理',ttimes:5,tschool:'國立台灣大學商研所',tjobname:'Jobs雜誌 專欄作家',industrytype:'電子科技',jobclasstype:'管理幕僚',tdescription:'只要透過深入訪談',timg:'../images/resumeReview/4.jpg',tlink:'../dist/consultant.html'},
-            
         ],
 
     },
@@ -33,19 +65,19 @@ new Vue({
         closereviewcard(){
             this.resumecardshow =!this.resumecardshow
         },
-        
-        industrytypeis(industrytypechoose){
-            this.industrytype = industrytypechoose;
+
+        industrytypeis(childindustrytype){
+            this.industrytype = childindustrytype;
             this.isshow1 = !this.isshow1
         },
 
-        jobclasstypeis(jobclasstypechoose){
-            this.jobclasstype = jobclasstypechoose;
+
+        jobclasstypeis(childjobclass){
+            this.jobclasstype = childjobclass;
             this.isshow2 = !this.isshow2
-        },
+        }
 
     
-
     },
 
     mounted() {
