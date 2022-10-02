a4fd55770eb3e27775888b7f96d1a5dadcbd90d7
theell3 <the.ell3.com@gmail.com>
2022-10-02 18:27:06 +0800

    學生會員編輯及更新串接資料庫  by chen

--- js/student_edit.js
+++ js/student_edit.js
@@ -2,5 +2,19 @@
 let save = document.querySelector(".save");
 
 save.addEventListener("click", () => {
-    location = "./student_main.html";
+    fetch(`php/updateEditUser.php`, {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify({
+            stuid : sessionStorage.getItem('StudentId'),
+            PHONE : phone.innerText,
+            EMAIL : email.innerText,
+            PASSWORD : password.innerText,
+            ADDRESS : address.innerText
+        })
+    })
+    .then (res => res.json())
+    .then (body => {})
+    window.location.assign(window.location.href);
 })
+
