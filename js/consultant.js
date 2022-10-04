a472903a0a139872a17c603162a72d0b73b45543
theell3 <the.ell3.com@gmail.com>
2022-10-04 15:33:42 +0800

    將consultant.js的PHP路徑改成./ BY CHEN

--- js/consultant.js
+++ js/consultant.js
@@ -42,7 +42,7 @@ new Vue({
 
             // 依照sessionStorage的StudentId(登入會員ID)去抓該會員的履歷 
 
-            fetch(`../php/resumeReviewcard.php?StudentId=${Number(sessionStorage.getItem('StudentId'))}`) //從後端JS拿到資料
+            fetch(`./php/resumeReviewcard.php?StudentId=${Number(sessionStorage.getItem('StudentId'))}`) //從後端JS拿到資料
             .then(rsp => rsp.json())
             .then(userArr => {            // userArr 是抓 resumeReviewcard.php 的 echo json_encode($resumeList);
                 this.resumeall= userArr   // resumeall 的data 抓userArr
